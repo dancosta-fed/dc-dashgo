f4e8a4a4ddbbd756e1db5e22502d1f45e741b924
Dan Costa <dancosta.id@gmail.com>
2022-04-22 16:37:38 -0300

    Revert "Query"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,14 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SideBarDrawerProvider } from '../contexts/SidebarDrawerContext'
-import { makeServer } from '../services'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-
-if (process.env.NODE_ENV !== 'production') {
-  makeServer()
-}
-
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <SideBarDrawerProvider>
-          <Component {...pageProps} />
-        </SideBarDrawerProvider>
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <SideBarDrawerProvider>
+        <Component {...pageProps} />
+      </SideBarDrawerProvider>
+    </ChakraProvider>
     )
 }
 
