8db3ccb3fadf6fecffa7eaea77814971cb566008
Daniel Costa <dancosta.id@gmail.com>
2022-04-22 09:18:37 -0300

    I've added miragejs and fakerjs. That way I can work with some fake users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SideBarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services'
+
+if (process.env.NODE_ENV !== 'production') {
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
